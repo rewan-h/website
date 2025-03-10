c68fc9fde02a4f60a914844feb107eddafaa204b
rewan-h <rewan.h@mackcollege.school.nz>
2025-03-11 12:58:19 +1300

    Finishing touches + How and Why pages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', Math.min(window.pageYOffset / (document.body.offsetHeight - window.innerHeight), 1));
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
+  
